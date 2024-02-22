41a627c2c91902784d72ce8aec2cfa50e0568c4a
ajchamp001 <adityagupta292001@gmail.com>
2024-02-22 16:10:34 +0530

    first commit

---
+++ javaScript.js
@@ -0,0 +1,62 @@
+const Base_url ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+const dropdown =  document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+
+// Access the data that i.s counrty list from another File(country.js)
+for( let select of dropdown){
+    for (currcode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText=currcode; 
+        newOption.value= currcode;
+        if (select.name === "from" && currcode === "USD"){
+            newOption.selected = "selected";
+        }else if(select.name === "to" && currcode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change",(evt) =>{
+        updateflag(evt.target);
+    });
+}
+
+const uptaeExchangeRate = async()=>{
+    let amount = document.querySelector(".amount input");
+    let amtvalue = amount.value;
+    if(amtvalue ==="" || amtvalue <1){
+        amtvalue = 1;
+       amtvalue.value ="1"
+    }
+
+
+    let URL = `${Base_url}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
+    let finalAmount = amtvalue*rate;
+    msg.innerText = `${amtvalue} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+
+}
+
+const updateflag=(element) =>{
+    let currcode = element.value;
+    let counrtycode = countryList[currcode]; // IN ,EU
+    let newsrc = `https://flagsapi.com/${counrtycode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newsrc;
+};
+
+
+btn.addEventListener("click",(evt)=>{
+    evt.preventDefault();
+    uptaeExchangeRate();
+});
+
+window.addEventListener("load",()=>{
+    uptaeExchangeRate();
+});
